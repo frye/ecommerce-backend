e2bf29630c8f8906ea39b3eeb5795b3b6e9aae64
Samuli Jarvinen <frye@github.com>
2022-03-21 20:22:13 -0700

    Finetuned the product routes

--- models/index.js
+++ models/index.js
@@ -6,16 +6,16 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
-Product.belongsTo(Category);
+Product.belongsTo(Category, {foreignKey: 'category_id'});
 
 // Categories have many Products
 Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: 'product_tag'});
+Product.belongsToMany(Tag, {through: 'product_tag', foreignKey: 'product_id'});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: 'product_tag'});
+Tag.belongsToMany(Product, {through: 'product_tag', foreignKey: 'product_id' });
 
 module.exports = {
   Product,
